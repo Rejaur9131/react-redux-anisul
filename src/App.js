8ce59c9d5af27e2de2b028d1784285d37c3968b8
MD REJAUR RAHMAN <mdrejaurrahman@MDs-MacBook-Pro.local>
2022-07-27 22:45:44 -0700

    counter component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Counter from "./components/counter/Counter";
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to react-redux</h1>
+      <Counter></Counter>
     </div>
   );
 }
---
+++ src/components/counter/Counter.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Counter = () => {
+  return (
+    <div>
+      <h2>Count: 0</h2>
+    </div>
+  );
+};
+
+export default Counter;
